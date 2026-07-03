4088257777c0c5117f6d01958542ca7a2501d387
marilynniere98@gmail.comgit config --global user.name <brentwaxx@gmail.com>
2026-07-03 00:42:08 -0700

    add 3rd number

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function jsCalc(){
     const n1 = document.getElementById('num1').value;
     const n2 = document.getElementById('num2').value;
+    const n3 = document.getElementById('num3').value;
 
     const number1 =parseFloat(n1);
     const number2 =parseFloat(n2);
+    const number3 =parseFloat(n3);
 
     //get the sum
-    const sum = number1 + number2;
+    const sum = number1 + number2 + number3;
 
     document.getElementById('answer').textContent=sum;
 }
